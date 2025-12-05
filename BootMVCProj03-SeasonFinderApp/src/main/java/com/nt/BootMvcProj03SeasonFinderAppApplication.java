e29a1fc0b74051875f1a37362f3a361a38e0ac47
Ritesh <pradhanritesh106@gmail.com>
2025-12-06 03:13:59 +0530

    changed

--- BootMVCProj03-SeasonFinderApp/src/main/java/com/nt/BootMvcProj03SeasonFinderAppApplication.java
+++ BootMVCProj03-SeasonFinderApp/src/main/java/com/nt/BootMvcProj03SeasonFinderAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootMvcProj03SeasonFinderAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("JENKINS PROGRAM SUCESSESFULL");
 		SpringApplication.run(BootMvcProj03SeasonFinderAppApplication.class, args);
 	}
 
